c343e09ba8e26e06280e491932859d1cd9dfea60
Marc Ocampo <mdocampo1@gmail.com>
2022-11-07 04:37:02 +0800

    align output with bash sha1sum tool for easier comparison

--- FileParser.cpp
+++ FileParser.cpp
@@ -5,17 +5,18 @@
 namespace input
 {
 
-FileParser::FileParser(const char* input) : _size(0)
+FileParser::FileParser(const char* input)
+  : _size(0)
 {
   _file.clear();
-  
-  std::ifstream sizeCountingStream(input, std::ifstream::binary);
+
+  std::ifstream sizeCountingStream(input);
   sizeCountingStream.seekg(0, std::ios::end);
 
   _size = static_cast<uint64_t>(sizeCountingStream.tellg());
   sizeCountingStream.close();
 
-  std::ifstream fileStream(input, std::ifstream::in);
+  std::ifstream fileStream(input);
   if(!fileStream.eof() && !fileStream.fail())
   {
     _file.resize(_size);
--- main.cpp
+++ main.cpp
@@ -7,12 +7,10 @@ constexpr auto VALID_NUM_ARG = 2;
 
 void printHash(const std::array<uint32_t, sha1::ESizeH>& hash)
 {
-  std::cout << "SHA1: ";
   for(auto it = hash.begin(); it != hash.end(); ++it)
   {
     std::cout << std::hex << *it;
   }
-  std::cout << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -20,10 +18,15 @@ int main(int argc, char** argv)
   if(VALID_NUM_ARG != argc)
   {
     std::cout << "ERR: Invalid commandline format. Please use \"./out <test_file>\"" << std::endl;
-    return 0;
+    return 1;
   }
 
   input::FileParser parser(argv[1]);
   sha1::MessageDigestCalculator sha1Calc(parser.getFile());
+
+  // aligns to bash sha1sum output
   printHash(sha1Calc.getMsgDigest());
+  std::cout << argv[1] << std::endl;
+
+  return 0;
 }
